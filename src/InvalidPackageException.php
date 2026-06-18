1a80c7b4b8180d28714ef573b3ce532072948c48
HungNth <thienhungnth@gmail.com>
2026-06-18 17:29:22 +0700

    feat: introduce InvalidPackageException for handling parsing errors

---
+++ src/InvalidPackageException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WpPackageParser;
+
+use RuntimeException;
+
+/**
+ * Exception thrown when the server fails to parse a plugin/theme.
+ */
+class InvalidPackageException extends RuntimeException {}
